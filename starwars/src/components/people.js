af1119bd09bbb454d95412868be3037d4d8d97e4
QuintonStudent <49251721+QuintonStudent@users.noreply.github.com>
2019-07-19 13:10:14 -0400

    added styling to cards class and people component

--- starwars/src/components/people.js
+++ starwars/src/components/people.js
@@ -2,19 +2,23 @@ import React from 'react';
 import styled from 'styled-components';
 
 const PeopleDiv = styled.div`
-  background-color: pink;
-
+  background-color: #000000;
+  width: 300px;
+  margin: 25px;
+  font-size: 1.5rem;
+  padding: 10px;
+  border-radius: 8px;
+  color: #ffe81f;
 `;
 
 const People = ({ propName, propHeight, propHairColor, propEyeColor }) => {
   return (
-    <PeopleDiv>
-      <div>{propName}</div>
-      <div>{propHeight} cm</div>
-      <div>{propHairColor}</div>
-      <div>{propEyeColor}</div>
-      <p></p>
-    </PeopleDiv>
+      <PeopleDiv>
+        <div>{propName}</div>
+        <div>Height: {propHeight} cm</div>
+        <div>Hair color: {propHairColor}</div>
+        <div>Eye color: {propEyeColor}</div>
+      </PeopleDiv>
   );
 };
 
